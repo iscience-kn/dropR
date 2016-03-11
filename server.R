91a4d0cc02ca55b776cc8827b9a14cb2c44a65f1
Matthias Bannert <matthias.bannert@gmail.com>
2016-03-11 08:54:26 +0100

    initial commit of dropR revamp

---
+++ server.R
@@ -0,0 +1,20 @@
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+  
+  output$table <- DT::renderDataTable(DT::datatable({
+    upfile <- read.csv2(input$file1$datapath,
+                        sep = input$sep,
+                        dec = input$dec,
+                        quote = input$quote,
+                        header = input$header)
+    
+    upfile
+  }
+  ))
+}
---
+++ ui.R
@@ -0,0 +1,91 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+
+# tab items ####
+tabHome <- tabItem(tabName = "home",
+                   h1("dropR"),
+                   h2("Drop-out analysis using R"))
+
+tabViz <- tabItem(tabName = "viz",
+                  fluidRow(
+                    box(plotOutput("plot1", height = 250)),
+                    box(title = "Controls",
+                        sliderInput("slider", "Number of observations:",
+                                    1, 100, 50)
+                        )
+                    )
+                  )
+
+tabUpload <- tabItem(tabName = "upload",
+                      h2("Upload your data"),
+                     fluidRow(
+                       box(
+                         h3("Choose .csv file"),
+                         tags$ul(
+                           tags$li("Indicate whether the first line of your
+                                   data is meant to be a header."),
+                           tags$li("Choose the proper column delimiter and
+                                   text quote for your file."),
+                           tags$li("Make sure to use reasonable coding for
+                                   missing values (i.e. empty cells). Avoid
+                                   ambigous coding such as -99, -999 etc."),
+                           tags$li("Check the preview window below. Iff your
+                                   is displayed as expected you are good to 
+                                   start with your analysis.")
+                         ),
+                         fileInput('file1', '',
+                                     accept=c('text/csv', 
+                                              'text/comma-separated-values,text/plain', 
+                                              '.csv'))
+                         ),
+                       box(
+                         h3("Specify .csv properties"),
+                         checkboxInput('header', 'Header', TRUE),
+                         radioButtons('sep', 'Separator',
+                                      c(Comma=',',
+                                        Semicolon=';',
+                                        Tab='\t'),
+                                      ','),
+                         radioButtons('quote', 'Text quote',
+                                      c(None='',
+                                        'Double quote'='"',
+                                        'Single quote'="'"),
+                                      '"')
+                       )
+                     ),
+                     fluidRow(
+                       box(width = 12,
+                         h3("Data Preview"),
+                         DT::dataTableOutput("table")
+                       )
+                     )
+                     )
+
+tabAnalysis <- tabItem(tabName = "viz",
+                       h2("graph"))
+
+
+
+
+# Main Page structure ####
+ui <- dashboardPage(
+  dashboardHeader(title = "dropR"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Upload", tabName = "upload", icon = icon("upload")),
+      menuItem("Visual analysis", tabName = "viz",
+               icon = icon("area-chart",lib="font-awesome"))
+    )
+  ),
+  # Body of the App #############
+  dashboardBody(
+    tabItems(tabHome,
+             tabViz,
+             tabUpload,
+             tabAnalysis)
+  )
+)
+
