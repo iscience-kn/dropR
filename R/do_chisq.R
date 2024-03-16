daca0eeee36971229c9d9f244d8cb429a0539202
Matt Bannert <bunny@hey.com>
2024-03-16 23:16:16 +0100

    more modularising of shiny app

---
+++ R/do_chisq.R
@@ -0,0 +1,28 @@
+#' Compute Chisq-Test Given a Question Position
+#' 
+#' 
+#' 
+#' @param d data.frame stats table computed by compute_stats.
+#' @importFrom stats chisq.test
+#' @export
+do_chisq <- function(d,
+                     chisq_question,
+                     sel_cond_chisq,
+                     fisher){
+  d <- subset(d, condition %in% sel_cond_chisq)
+  d$condition <- factor(d$condition)
+  
+  # d <- subset(d,condition != "total")
+  
+  test_input <- subset(d, do_idx == chisq_question)
+  test_table <- as.table(as.matrix(test_input[,c("cs","remain")]))
+  dimnames(test_table) <- list(conditions = test_input$condition,
+                               participants = c("dropout","remaining"))
+  # chisq.test(as.table(as.matrix(test_input[,c("condition","cs","remain")])))
+  test_result <- chisq.test(test_table, simulate.p.value = fisher)
+  lname2 <- sprintf("Dropout at question %s", chisq_question)
+  li <- list("Test result" = test_result,
+             lname2 = test_table)
+  names(li)[2] <- lname2
+  li
+}
---
+++ R/do_or_table.R
@@ -0,0 +1,19 @@
+#' @param d data.frame stats table as computed by compute_stats.
+#' @export
+do_or_table <- function(d,
+                        chisq_question,
+                        sel_cond_chisq){
+  d <- as.data.frame(stats())
+  d <- subset(d,condition %in% sel_cond_chisq)
+  d$condition <- factor(d$condition)
+  # d <- subset(d,condition != "total")
+  
+  test_input <- subset(d, do_idx == chisq_question)
+  
+  OR_matrix <- outer(test_input$pct_remain,
+                     test_input$pct_remain,
+                     FUN = get_odds_ratio)
+  colnames(OR_matrix) <- test_input$condition
+  row.names(OR_matrix) <- test_input$condition
+  OR_matrix
+}
