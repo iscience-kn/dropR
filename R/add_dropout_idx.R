f8fbb992690d8511e7f032fd5d4f8bc8770dd465
the-tave <you@example.comoverland@uni-bremen.de>
2024-05-23 16:11:48 +0200

    made idx calculation easier and more precise

--- R/add_dropout_idx.R
+++ R/add_dropout_idx.R
@@ -7,16 +7,18 @@
 #' @param q_pos numeric columns that contain questions
 #' @export
 add_dropout_idx <- function(df, q_pos){
-  nms <- names(df[,q_pos])
-  tf <- is.na(df[,q_pos])
-  tpos <- apply(tf, 1, which)
-  tpos[lapply(tpos, length) == 0] <- NA
-  # cover unit NR
-  tpos[lapply(tpos, length) == length(q_pos)] <- 1
-
-  # out vector contains drop out position
-  # dpos <- 
-  df$do_idx <- sapply(tpos, find_drop_out, clnms = nms)
+  # nms <- names(df[,q_pos])
+  # tf <- is.na(df[,q_pos])
+  # tpos <- apply(tf, 1, which)
+  # tpos[lapply(tpos, length) == 0] <- NA
+  # # cover unit NR
+  # tpos[lapply(tpos, length) == length(q_pos)] <- 1
+  # 
+  # # out vector contains drop out position
+  # # dpos <-
+  # df$do_idx <- sapply(tpos, find_drop_out, clnms = nms)
+  df$do_idx <- rowSums(sapply(df[, q_pos], !is.na))
+  
   df
 }
 
