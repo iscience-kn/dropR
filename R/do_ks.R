d3a4402eb81447211e55c9fc8434687013ad1e6c
the-tave <you@example.comoverland@uni-bremen.de>
2024-05-23 12:12:54 +0200

    added Kolmogorov Test ver 0.1; worked on improving compute_stats

---
+++ R/do_ks.R
@@ -0,0 +1,26 @@
+#' Compute Kolmogorov-Smirnov Test for most extreme conditions
+#'
+#' @param data A dataset made from dropR::compute_stats(), containing information on the percent remaining per question per condition
+#' @param questions Number of questions to be included in analysis
+#'
+#' @return
+#' @export
+#'
+#' @examples do_ks(stats, 51)
+do_ks <- function(data, questions){
+  library(dplyr)
+  
+  extremes <- data %>% 
+    filter(do_idx == questions, #length(input$quest_cols)-1
+           condition != "total") %>% 
+    filter(pct_remain == max(pct_remain) | pct_remain == min(pct_remain)) %>% 
+    pull(condition)
+  
+  # extreme_data <- stats %>% 
+  #   filter(condition %in% extremes)
+  # stats$pct_remain[stats$condition == extremes[1]]
+  
+  ks.test(x = stats$pct_remain[stats$condition == extremes[1]],
+          y = stats$pct_remain[stats$condition == extremes[2]])
+  
+}
