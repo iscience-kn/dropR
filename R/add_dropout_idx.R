133451d1a59aa95d629e828d97d2512025fe17fa
the-tave <you@example.comoverland@uni-bremen.de>
2024-05-23 18:50:20 +0200

    added a check for single NAs that are not DO

--- R/add_dropout_idx.R
+++ R/add_dropout_idx.R
@@ -17,7 +17,22 @@ add_dropout_idx <- function(df, q_pos){
   # # out vector contains drop out position
   # # dpos <-
   # df$do_idx <- sapply(tpos, find_drop_out, clnms = nms)
-  df$do_idx <- length(q_pos) - rowSums(sapply(df[, q_pos], is.na))
+  df$do_idx <- length(q_pos) - rowSums(sapply(df[, q_pos], is.na)) # length(q_pos) - 
+  
+  # identify single missing by using block entropy
+  ent <- NA
+  for(i in 1:nrow(df)){
+    x <- q_pos - which(is.na(df[i, ]))
+    
+    p_xi <- table(x) / length(x)
+    result <- sum(p_xi * log2(p_xi)) * (-1)
+    
+    ent[i] <- result
+  }
+  
+  # df$do_idx[which(ent > 1)] <- 0 # length(q_pos)
+  # df$do_idx[df$do_idx == length(q_pos)] <- 0
+  df$do_idx[which(df$do_idx == length(q_pos) | ent > 1)] <- 0
   
   df
 }
