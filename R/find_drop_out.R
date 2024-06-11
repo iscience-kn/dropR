093a52bf34b331cc0dc58eae333c5b632366cac8
the-tave <you@example.comoverland@uni-bremen.de>
2024-06-11 13:11:55 +0200

    added lifecycle to the pacakge as good practice

--- R/find_drop_out.R
+++ R/find_drop_out.R
@@ -7,10 +7,13 @@
 #' `find_drop_out()` is deprecated in favor of [add_dropout_idx()], which is the 
 #' basis for dropout analysis using `dropR`.
 #' 
+#' For reliable dropout analysis, please use [add_dropout_idx()] instead.
+#' 
 #' @param v a vector
-#' @param clnms specify the parts that actually hold questions by character names.
+#' @param clnms character. Columns of question items in the vector that actually hold questions.
 #' @export
 find_drop_out <- function(v,clnms){
+  lifecycle::deprecate_warn("1.0", "find_drop_out()", "add_dropout_idx()")
   # length of the vector of question labels
   l <- length(clnms)
   # create backwards sequence
